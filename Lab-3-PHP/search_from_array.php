bcaa237fd148cd3b03e11c761022439e0ad71ce7
Jaber-SB <159601821+Jaber-SB@users.noreply.github.com>
2024-11-17 13:50:25 +0600

    Add files via upload

---
+++ Lab-3-PHP/search_from_array.php
@@ -0,0 +1,9 @@
+$array = [10, 20, 30, 40, 50];
+$search = 30;
+
+if (in_array($search, $array)) {
+    echo "$search is found in the array\n";
+} else {
+    echo "$search is not found in the array\n";
+}
+?>
